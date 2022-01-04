c79292cbb3e4b745191276df95df1b02e4e5fdea
SARAV <sarav023b@gmail.com>
2022-01-04 22:33:41 +0530

    Changed connection status

--- config/db.js
+++ config/db.js
@@ -3,7 +3,14 @@ require("dotenv").config();
 const databaseURL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.8sjto.mongodb.net/Tradego-mob?retryWrites=true&w=majority`;
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(databaseURL);
+    const conn = await mongoose.connect(databaseURL, {
+      useNewUrlParser: true,
+      writeConcern: {
+        j: true,
+        wtimeout: 50000,
+      },
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected : ${conn.connection.host}`);
     console.log("Database Name::" + conn.connections[0].name);
   } catch (err) {
